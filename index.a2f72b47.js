db5338b970b6ffa66b8b2f82d7d157643eee1a88
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-01-09 13:08:23 +0000

    Deploying to gh-pages from @ mariastetciuk/Donut-Masterclass@0c924c1fe9d3d00756159f9a797f7314bcc8d4b3 🚀

---
+++ index.a2f72b47.js
@@ -0,0 +1,2 @@
+new Swiper(".reviews__slider",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",type:"fraction"},grabCursor:!0,slideToClickedSlide:!0,keyboard:{enadled:!0,onlyInViewport:!0,pageUpDown:!0},centeredSlides:!0,loop:!0,breakpoints:{320:{slidesPerView:1},768:{slidesPerView:3}}});
+//# sourceMappingURL=index.a2f72b47.js.map
